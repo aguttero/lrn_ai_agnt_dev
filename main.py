1f8858b05ce5715bcb89ac0d367077d3c5646302
aguttero <aguttero@gmail.com>
2026-05-14 12:27:32 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from lrn-ai-agnt-dev!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ udemy_gen_ai_engineer_bootcamp_notes.md
@@ -0,0 +1,4 @@
+# Udemy Course:
+https://www.udemy.com/course/ai-developer-bootcamp/
+
+# Tutorial Notes:
