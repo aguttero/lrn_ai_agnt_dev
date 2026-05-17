738b3b2681808884f654933e83fb32fac138a3ff
aguttero <aguttero@gmail.com>
2026-05-17 07:39:30 -0400

    0517am01 InMemory example

--- agent_memory_db_loop.py
+++ agent_memory_db_loop.py
@@ -1,28 +1,12 @@
-#from langchain.chat_models import init_chat_model
-from typing import Tuple
 import requests
 from dotenv import dotenv_values
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-# from dotenv import load_dotenv
 from langchain.agents import create_agent
+from langgraph.checkpoint.memory import InMemorySaver
 
 config = dotenv_values(".env")
-API_KEY = config.get("GOOGLE_API_KEY","default_value")
-
-# SIMPLE LLM INVOKE
-# from langchain.chat_models import init_chat_model
-# Init Gemini 3 Flash Preview
-# model = init_chat_model(
-#     model="gemini-3-flash-preview", 
-#     model_provider="google-genai", 
-#     api_key=API_KEY)
-
-# def invoke (user_prompt):
-#     with open ("data/sail_material.txt","r") as file:
-#         file_content = file.read()
-#     final_prompt = f"{user_prompt}: {file_content}"
-#     return model.invoke(final_prompt)
+GOOGLE_API_KEY = config.get("GOOGLE_API_KEY","default_value")
 
 # TOOL FUNCTION EXAMPLE
 # Is important to enforce typing hints so LLM knows how to call the function
@@ -47,15 +31,16 @@ def get_weather(city: str):
     #         "rain forecast": 0.90}
 
 
-def get_location() -> Tuple[str,str]:
-    """Returns user location: City Name and Country ISO code in a tuple (city_name, country_iso_code) of the user loccation""" # AI Agent reads the function information to decide if it is useful
-    return "Buenos Aires", "AR"
+def get_location() -> dict:
+    """Returns user location: City Name and Country ISO code in a dictionary with the user loccation""" # AI Agent reads the function information to decide if it is useful
+    return {"city": "Buenos Aires",
+            "country_iso_code": "AR"}
 
 # Initialize Gemini Flash 2.5
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0.7,
-    api_key=API_KEY
+    api_key=GOOGLE_API_KEY
 )
 # LLM input format: String
 # LLM output response object instance (response.content, response.response_metadata, response.usage_metadata, etc)
@@ -67,62 +52,57 @@ YOUR WORKFLOW:
 1. If the user asks about weather WITHOUT specifying a location, you MUST:
    - First call get_location() to find their location
    - Then call get_weather(city) with that location
-   
+
 2. If the user provides a city, call get_weather(city) directly.
-
-3. Use your knowledge to returno the temperature in the metric unit that is standard for the given location.
-
-4. Present the weather information including temperature, condition, wind speed, wind chill, humidity, and any other relevant details.
 """
 
 # Initialize Agent
 agent = create_agent(
     model=llm,
     tools=[get_weather,get_location],
-    system_prompt=system_prompt
+    system_prompt=system_prompt,
+    checkpointer=InMemorySaver()
 )
 # Agent Input format: Dictionary
 # Agent Output: Dictionary {"messages": [HumanMessage object(), AiMessage object()]} 
 
 
 def show_api_key():
-    print(f"API KEY={API_KEY}")
+    print(f"GOOGLE_API KEY={GOOGLE_API_KEY}")
+    return GOOGLE_API_KEY
 
 
 def main():
-    print(f"API KEY={API_KEY}")
     print("Hello from lrn-ai-agnt-dev!")
-
+    print(".env data:", show_api_key())
     print ("- - - -")
     
-    user_query = input("Enter your query: ")
-    # user_query = "How is the weather?" 
-    
+    # Query 1
+    user_query_1 = input("Enter your query: ")
     agent_prompt = {
         "messages":[
             {"role": "user",
-             #"content": "How is the weather in Santiago de Chile?" 
-             #"content": "How is the weather?" 
-             "content": user_query 
+             "content": user_query_1
              }
         ]
     } 
-
-    response0 = agent.invoke(agent_prompt)
-    print (f"agent_prompt={agent_prompt}")
+    response1 = agent.invoke (agent_prompt, {"configurable": {"thread_id": "1"}})
+    print(response1["messages"][-1].content)
     print ("- - - -")
-    print("ITEM [-1]:")
-    print(response0["messages"][-1].content)
-    print ("- - - -")
-    # print("FULL RESPONSE:")
-    # print(response0)
 
-    # SIMPLE LLM INVOKE
-    # llm_prompt = "Which are your main directives? Who trained you?"
-    # response1 = llm.invoke(llm_prompt)
-    # print (f"llm_prompt={llm_prompt}")
-    # print(response1.content)
-    # # print(response1) # contains token usage detail and other data
+    # Query 2
+    user_query_2 = input("Enter your query: ")
+    agent_prompt = {
+        "messages":[
+            {"role": "user",
+             "content": user_query_2
+             }
+        ]
+    } 
+    response2 = agent.invoke (agent_prompt, {"configurable": {"thread_id": "1"}})
+    print(response2["messages"][-1].content)
+    print ("- - - -")
+    
 
     print ("- - END - - ")
 
--- udemy_gen_ai_engineer_bootcamp_notes.md
+++ udemy_gen_ai_engineer_bootcamp_notes.md
@@ -2,3 +2,19 @@
 https://www.udemy.com/course/ai-developer-bootcamp/
 
 # Tutorial Notes:
+* modulo langchain permite hablar con LLMs
+
+# LLM.invoke y agent.invoke
+* see code `a_basic_LLM_call.py`
+* see code `a_basic_agnt_call.py`
+
+# memory
+## modules
+* import InMemorySaver permite al agente crear la 'memoria'
+* modulo langraph.checkpoint.memory 
+* see code `agent_memory_db.py`
+
+## code
+* agent.invoke {"configurable":{"thread_id":"n"}} es lo que identifica el thread para la memoria
+* thread_id gets associated with each user in your app to have a memory for each conversation
+
